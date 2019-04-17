ffa78a4758b31ac38691a50fb64d6106c7cbca29
Vilma M <vilma.medrano00@gmail.com>
2019-04-16 23:00:00 -0700

    add constraint for guess to be 1 to 100, implment in while loop and ask to guess again

--- game.py
+++ game.py
@@ -8,15 +8,15 @@ our_number = randint(0,100)
 player_num = int(input('Guess a number between 1 and 100: '))
 number_of_guesses = 1
 
-if 1 <= player_num <= 100:
-    while player_num != our_number:
-        if player_num > our_number:
-            player_num = int(input('Too high! Guess another number: '))
-            number_of_guesses += 1
-        elif player_num < our_number:
-            player_num = int(input('Too low! Guess another number: '))
-            number_of_guesses += 1
-    print(player_name + ' you got my number! In only ' + str(number_of_guesses) + ' guesses!')
-else:
-    print("that number ain't between 1 and 100, guess again")
-    continue
+while player_num != our_number:
+    if player_num > our_number and player_num <= 100:
+        player_num = int(input('Too high! Guess another number: '))
+        number_of_guesses += 1
+    elif player_num < our_number and player_num >= 1:
+        player_num = int(input('Too low! Guess another number: '))
+        number_of_guesses += 1
+    else:
+        player_num = int(input("that number ain't between 1 and 100! guess again: "))
+print(player_name + ' you got my number! In only ' + str(number_of_guesses) + ' guesses!')
+
+
