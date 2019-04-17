428be1fb2d2ff196c9a114c5ba03fc0cf0bee7a5
Vilma M <vilma.medrano00@gmail.com>
2019-04-16 22:01:27 -0700

    remove unnecessary code

--- game.py
+++ game.py
@@ -8,14 +8,13 @@ our_number = randint(0,100)
 player_num = int(input('Guess a number between 1 and 100: '))
 number_of_guesses = 1
 
+
 while player_num != our_number:
     if player_num > our_number:
         player_num = int(input('Too high! Guess another number: '))
         number_of_guesses += 1
-        continue
     elif player_num < our_number:
         player_num = int(input('Too low! Guess another number: '))
         number_of_guesses += 1
-        continue
 
 print(player_name + ' you got my number! In only ' + str(number_of_guesses) + ' guesses!')
