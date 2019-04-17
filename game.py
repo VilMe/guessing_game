3b5334a4fd20946179f98cfd7cbe7fce418c3f6e
Vilma M <vilma.medrano00@gmail.com>
2019-04-17 13:32:17 -0700

    try and except error handling, implement ValueError handling for user input when not an integer, loop not working correctly yet

--- game.py
+++ game.py
@@ -5,18 +5,23 @@ from random import randint
 print('Hi!')
 player_name = input('What is you name? ') 
 our_number = randint(0,100)
-player_num = int(input('Guess a number between 1 and 100: '))
+player_num = 0
 number_of_guesses = 1
 
 while player_num != our_number:
-    if player_num > our_number and player_num <= 100:
-        player_num = int(input('Too high! Guess another number: '))
-        number_of_guesses += 1
-    elif player_num < our_number and player_num >= 1:
-        player_num = int(input('Too low! Guess another number: '))
-        number_of_guesses += 1
-    else:
-        player_num = int(input("that number ain't between 1 and 100! guess again: "))
+	try:
+		player_num = int(input('Guess a number between 1 and 100: '))
+		if player_num > our_number and player_num <= 100:
+			player_num = int(input('Too high! Guess another number: '))
+			number_of_guesses += 1
+		elif player_num < our_number and player_num >= 1:
+			player_num = int(input('Too low! Guess another number: '))
+			number_of_guesses += 1
+		else:
+			player_num = int(input("that number ain't between 1 and 100! guess again: "))
+	except ValueError:
+		player_num = int(input("that's not a valid number, buddy, try again: "))
 print(player_name + ' you got my number! In only ' + str(number_of_guesses) + ' guesses!')
+
 
 
