a9407c566a365b1a5b8aeb6950d24e98d9387ea4
Vilma M <vilma.medrano00@gmail.com>
2019-08-08 17:34:23 -0700

    fix test for input, move non informational text input (no high or low indicator) to be initial number, instead of zero

--- game.py
+++ game.py
@@ -5,12 +5,11 @@ from random import randint
 print('Hi!')
 player_name = input('What is you name? ') 
 our_number = randint(0,100)
-player_num = 0
+player_num = int(input('Guess a number between 1 and 100: '))
 number_of_guesses = 1
 
 while player_num != our_number:
 	try:
-		player_num = int(input('Guess a number between 1 and 100: '))
 		if player_num > our_number and player_num <= 100:
 			player_num = int(input('Too high! Guess another number: '))
 			number_of_guesses += 1
